58940e40a082f30c51c2b56d57c1a500f1869fb5
Lijian <1033526540@qq.com>
2020-06-23 14:29:09 +0800

    add module run guide

---
+++ module_b/src/main/java/debug/ModuleBApplication.java
@@ -0,0 +1,15 @@
+package debug;
+
+
+import android.app.Application;
+
+/**
+ * B模块单独运行时的壳
+ */
+public class ModuleBApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
